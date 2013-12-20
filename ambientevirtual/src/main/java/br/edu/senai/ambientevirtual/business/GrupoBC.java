b71d715cb727eb688cf7be896ee2fbb16b05c706
william.chenta <william.chenta@dd937653-172c-c5d0-5884-f009d19de3e6>
2013-12-20 11:13:40 +0000

    William - Implementando crud de grupos

--- ambientevirtual/src/main/java/br/edu/senai/ambientevirtual/business/GrupoBC.java
+++ ambientevirtual/src/main/java/br/edu/senai/ambientevirtual/business/GrupoBC.java
@@ -1,14 +1,20 @@
 package br.edu.senai.ambientevirtual.business;
 
-import br.gov.frameworkdemoiselle.stereotype.BusinessController;
-import br.gov.frameworkdemoiselle.template.DelegateCrud;
+import java.util.List;
+import java.util.Map;
 
 import br.edu.senai.ambientevirtual.domain.Grupo;
 import br.edu.senai.ambientevirtual.persistence.GrupoDAO;
+import br.gov.frameworkdemoiselle.stereotype.BusinessController;
+import br.gov.frameworkdemoiselle.template.DelegateCrud;
 
 @BusinessController
 public class GrupoBC extends DelegateCrud<Grupo, Long, GrupoDAO> {
 	
 	private static final long serialVersionUID = 1L;
+	
+	public List<Grupo> filtrarQuery(String filtro, Map<String, String> params) {
+		return getDelegate().filtrarQuery(filtro, params);
+	}
 	
 }
--- ambientevirtual/src/main/java/br/edu/senai/ambientevirtual/persistence/GrupoDAO.java
+++ ambientevirtual/src/main/java/br/edu/senai/ambientevirtual/persistence/GrupoDAO.java
@@ -1,14 +1,48 @@
 package br.edu.senai.ambientevirtual.persistence;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.Query;
+
 import br.gov.frameworkdemoiselle.stereotype.PersistenceController;
 import br.gov.frameworkdemoiselle.template.JPACrud;
-
 import br.edu.senai.ambientevirtual.domain.Grupo;
+import br.edu.senai.ambientevirtual.domain.Turma;
 
 @PersistenceController
 public class GrupoDAO extends JPACrud<Grupo, Long> {
 
 	private static final long serialVersionUID = 1L;
-	
+
+	public List<Grupo> filtrarQuery(String tpFiltro, Map<String, String> params) {
+		String query = "Select g from Grupo g where g.nome = :nome";
+		
+		if ("turma".equals(tpFiltro)) {
+			query = "Select g from Grupo g where g.turma.codigo = :turma";
+		}
+		
+		if ("tutor".equals(tpFiltro)) {
+			query = "Select g from Grupo g where g.tutor.usuario.nome = :tutor";
+		}
+		
+		Query filtro = createQuery(query);
+		
+		for (Iterator<String> iterator = params.keySet().iterator(); iterator.hasNext();) {
+			String chave = iterator.next();
+			
+			if ("semestre".equals(tpFiltro)) {
+				filtro.setParameter(chave, Integer.parseInt(params.get(chave)));
+			}
+			else {
+				filtro.setParameter(chave, params.get(chave));
+			}
+		}
+		
+		params.clear();
+		
+		return (List<Grupo>) filtro.getResultList();
+	}
 
 }
