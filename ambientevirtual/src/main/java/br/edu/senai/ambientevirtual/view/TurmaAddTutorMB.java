6527d43ef03157d10772d7aa003c594503174bde
william.chenta <william.chenta@dd937653-172c-c5d0-5884-f009d19de3e6>
2013-12-20 17:28:51 +0000

    William - Implementando regra negócio de adcionar tutores a turma

--- ambientevirtual/src/main/java/br/edu/senai/ambientevirtual/view/TurmaAddTutorMB.java
+++ ambientevirtual/src/main/java/br/edu/senai/ambientevirtual/view/TurmaAddTutorMB.java
@@ -1,51 +1,59 @@
 package br.edu.senai.ambientevirtual.view;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.inject.Inject;
 
 import br.edu.senai.ambientevirtual.business.TurmaBC;
 import br.edu.senai.ambientevirtual.business.TutorBC;
 import br.edu.senai.ambientevirtual.domain.Turma;
 import br.edu.senai.ambientevirtual.domain.Tutor;
-import br.gov.frameworkdemoiselle.annotation.PreviousView;
-import br.gov.frameworkdemoiselle.stereotype.ViewController;
-import br.gov.frameworkdemoiselle.template.AbstractEditPageBean;
 
-@ViewController
-@PreviousView("./turma_list.jsf")
-public class TurmaAddTutorMB extends AbstractEditPageBean<Turma, Long> {
+@ManagedBean
+@SessionScoped
+public class TurmaAddTutorMB {
 
-	private static final long serialVersionUID = 1L;
 	@Inject
 	private TurmaBC turmaBC;
 	
 	@Inject
 	private TutorBC tutorBC;
 	
+	private List<Tutor> tutor = new ArrayList<Tutor>();
+	
+	private String parametro;
+	
+	public String getParametro() {
+		return parametro;
+	}
+
+	public void setParametro(String parametro) {
+		this.parametro = parametro;
+	}
+
+	public String outcome(){
+		return "turma_add_tutor";
+	}
+	
+	public List<Tutor> getTutor() {
+		return tutor;
+	}
+
+	public void setTutor(List<Tutor> tutor) {
+		this.tutor = tutor;
+	}
+
 	public List<Tutor> getTutores() {
 		return tutorBC.findAll();
 	}
 	
-	@Override
-	public String delete() {
-		return null;
+	public String salve() {		
+		Turma turma = turmaBC.load(Long.valueOf(parametro));
+		turma.setTutores(tutor);
+		turmaBC.update(turma);
+		return "turma_list";
 	}
-
-	@Override
-	public String insert() {
-		return null;
-	}
-
-	@Override
-	public String update() {
-		this.turmaBC.update(getBean());
-		return getPreviousView();
-	}
-
-	@Override
-	protected void handleLoad() {
-		setBean(this.turmaBC.load(getId()));
-	}
-	
 }
