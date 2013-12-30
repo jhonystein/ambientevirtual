257178276cf1b08465f3797271a44630f5238baa
william.chenta <william.chenta@dd937653-172c-c5d0-5884-f009d19de3e6>
2013-12-30 11:50:15 +0000

    William - Implementando resolução de atividade na visão do aluno

--- ambientevirtual/src/main/java/br/edu/senai/ambientevirtual/business/EntregaAtividadeBC.java
+++ ambientevirtual/src/main/java/br/edu/senai/ambientevirtual/business/EntregaAtividadeBC.java
@@ -1,18 +1,11 @@
 package br.edu.senai.ambientevirtual.business;
 
+import br.edu.senai.ambientevirtual.domain.EntregaAtividade;
+import br.edu.senai.ambientevirtual.persistence.EntregaAtividadeDAO;
 import br.gov.frameworkdemoiselle.stereotype.BusinessController;
 import br.gov.frameworkdemoiselle.template.DelegateCrud;
 
-import br.edu.senai.ambientevirtual.domain.EntregaAtividade;
-import br.edu.senai.ambientevirtual.persistence.EntregaAtividadeDAO;
-
 @BusinessController
 public class EntregaAtividadeBC extends DelegateCrud<EntregaAtividade, Long, EntregaAtividadeDAO> {
-	
 	private static final long serialVersionUID = 1L;
-	
-	@Override
-	public void insert(EntregaAtividade bean) {
-		getDelegate().insert(bean);
-	}
 }
