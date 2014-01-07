803fb23d05b7af636c557a6eec1ddd19583a0e1e
marcelo.schmitt1@gmail.com <marcelo.schmitt1@gmail.com@dd937653-172c-c5d0-5884-f009d19de3e6>
2014-01-07 16:25:31 +0000

    Tarefa 29 - Obter os dados do usuário logado.

--- ambientevirtual/src/main/java/br/edu/senai/ambientevirtual/business/UsuarioBC.java
+++ ambientevirtual/src/main/java/br/edu/senai/ambientevirtual/business/UsuarioBC.java
@@ -5,10 +5,15 @@ import br.gov.frameworkdemoiselle.template.DelegateCrud;
 
 import br.edu.senai.ambientevirtual.domain.Usuario;
 import br.edu.senai.ambientevirtual.persistence.UsuarioDAO;
+import br.edu.senai.ambientevirtual.security.Credenciais;
 
 @BusinessController
 public class UsuarioBC extends DelegateCrud<Usuario, Long, UsuarioDAO> {
 	
 	private static final long serialVersionUID = 1L;
+	
+	public Usuario buscaUsuarioCredenciais(Credenciais credenciais) {
+		return getDelegate().buscaUsuaroCredencial(credenciais);
+	}
 	
 }
