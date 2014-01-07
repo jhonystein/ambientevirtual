fe6425e9dceff538a92de42cfd1a6112217588ce
marcelo.schmitt1@gmail.com <marcelo.schmitt1@gmail.com@dd937653-172c-c5d0-5884-f009d19de3e6>
2014-01-07 16:25:18 +0000

    Tarefa 29 - Obter os dados do usuário logado.

--- ambientevirtual/src/main/java/br/edu/senai/ambientevirtual/persistence/UsuarioDAO.java
+++ ambientevirtual/src/main/java/br/edu/senai/ambientevirtual/persistence/UsuarioDAO.java
@@ -1,14 +1,32 @@
 package br.edu.senai.ambientevirtual.persistence;
 
+import javax.persistence.TypedQuery;
+
 import br.gov.frameworkdemoiselle.stereotype.PersistenceController;
 import br.gov.frameworkdemoiselle.template.JPACrud;
 
 import br.edu.senai.ambientevirtual.domain.Usuario;
+import br.edu.senai.ambientevirtual.security.Credenciais;
 
 @PersistenceController
 public class UsuarioDAO extends JPACrud<Usuario, Long> {
 
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * Busca o usuário com as dadas credenciais.
+	 * 
+	 * @param credenciais
+	 * 				as credenciais do usuário
+	 * @return o usuário encontrado ou null caso nenhum 
+	 * usuário tenha sido encontrado
+	 */
+	public Usuario buscaUsuaroCredencial(Credenciais credenciais) {
+		String jpql = "select u from Usuario u where u.login = :login and u.senha = :senha";
+		TypedQuery<Usuario> busca = getEntityManager().createQuery(jpql, getBeanClass());
+		busca.setParameter("login", credenciais.getLogin());
+		busca.setParameter("senha", credenciais.getSenha());
+		return busca.getSingleResult();
+	}
 
 }
