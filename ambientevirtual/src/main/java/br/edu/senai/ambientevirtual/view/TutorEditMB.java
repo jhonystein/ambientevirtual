9a5af231d4675a32f79589d90c124d83e34f8c5f
marcelo.schmitt1@gmail.com <marcelo.schmitt1@gmail.com@dd937653-172c-c5d0-5884-f009d19de3e6>
2014-01-10 17:02:22 +0000

    Chacagem de logins disponíveis para a tela de edição de tutores (TutorEditMB).

--- ambientevirtual/src/main/java/br/edu/senai/ambientevirtual/view/TutorEditMB.java
+++ ambientevirtual/src/main/java/br/edu/senai/ambientevirtual/view/TutorEditMB.java
@@ -10,6 +10,7 @@ import br.edu.senai.ambientevirtual.domain.Sexo;
 import br.edu.senai.ambientevirtual.domain.Tutor;
 import br.gov.frameworkdemoiselle.annotation.PreviousView;
 import br.gov.frameworkdemoiselle.security.RequiredRole;
+import br.gov.frameworkdemoiselle.security.SecurityContext;
 import br.gov.frameworkdemoiselle.stereotype.ViewController;
 import br.gov.frameworkdemoiselle.template.AbstractEditPageBean;
 import br.gov.frameworkdemoiselle.transaction.Transactional;
@@ -29,6 +30,9 @@ public class TutorEditMB extends AbstractEditPageBean<Tutor, Long> {
 	
 	@Inject
 	private FacesContext facesContext;
+	
+	@Inject
+	private SecurityContext securityContext;
 	
 	public Sexo[] getSexoValues() {
 		return Sexo.values();
@@ -62,14 +66,51 @@ public class TutorEditMB extends AbstractEditPageBean<Tutor, Long> {
 		setBean(this.tutorBC.load(getId()));
 	}
 	
+	/**
+	 * Se estiver em modo de inserção / cadastro, checa apenas se 
+	 * o login já existe.
+	 * 
+	 * Se estiver em modo update confere primeiramente a role do usuário.
+	 * Se for adm checa se o login já existe ou se ele é diferente do login 
+	 * atual do usuário a ter os dados atualizados.
+	 * Se não for adm checa se o login já existe ou se ele é diferente do login
+	 * do usuário logado.
+	 * 
+	 * Se alguma das condições for verdadeira então exibe uma mensagem de 
+	 * alerta e limpa o campo de login.
+	 */
 	@Transactional
 	public void checaLogin() {
-		if (usuarioBC.existeLogin(this.getBean().getUsuario().getLogin())) {
-			facesContext.addMessage("login", new FacesMessage(
-					FacesMessage.SEVERITY_WARN, "Login já existente. Tente um login diferente.", null));
-			getBean().getUsuario().setLogin("");
+		if (!isUpdateMode()) {
+			if (usuarioBC.existeLogin(this.getBean().getUsuario().getLogin())) {
+				facesContext.addMessage("login", new FacesMessage(
+						FacesMessage.SEVERITY_WARN, "Login já existente. Tente um login diferente.", null));
+				getBean().getUsuario().setLogin("");
+			} else {
+				facesContext.addMessage("login", new FacesMessage("OK"));
+			}
 		} else {
-			facesContext.addMessage("login", new FacesMessage("OK"));
+			if (securityContext.hasRole("adm")) {
+				if (usuarioBC.existeLogin(this.getBean().getUsuario().getLogin())
+						&& !getBean().getUsuario().getLogin().equals(
+								usuarioBC.load(getBean().getUsuario().getId()).getLogin())) {
+					facesContext.addMessage("login", new FacesMessage(
+							FacesMessage.SEVERITY_WARN, "Login já existente. Tente um login diferente.", null));
+					getBean().getUsuario().setLogin("");
+				} else {
+					facesContext.addMessage("login", new FacesMessage("OK"));
+				}
+			} else {
+				if (usuarioBC.existeLogin(this.getBean().getUsuario().getLogin())
+						&& !getBean().getUsuario().getLogin().equals(
+								((String) securityContext.getUser().getAttribute("login")))) {
+					facesContext.addMessage("login", new FacesMessage(
+							FacesMessage.SEVERITY_WARN, "Login já existente. Tente um login diferente.", null));
+					getBean().getUsuario().setLogin("");
+				} else {
+					facesContext.addMessage("login", new FacesMessage("OK"));
+				}
+			}
 		}
 	}
 	
