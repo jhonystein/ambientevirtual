5002e4c67f6437fecfe3e985a6c3453b6606563e
william.chenta <william.chenta@dd937653-172c-c5d0-5884-f009d19de3e6>
2014-01-15 13:32:16 +0000

    William - obtendo dados do usuário logado

--- ambientevirtual/src/main/java/br/edu/senai/ambientevirtual/domain/Turma.java
+++ ambientevirtual/src/main/java/br/edu/senai/ambientevirtual/domain/Turma.java
@@ -1,3 +1,12 @@
+/*
+* Ambiente Virtual
+* Copyright (C) 2014 SENAI
+* ----------------------------------------------------------------------------
+* Este arquivo é parte do Sistema Ambiente Virtual.
+* Finalidade - Criação de atividades para os alunos na visão dos tutores.
+* 
+* Criado por - William Chenta
+*/ 
 package br.edu.senai.ambientevirtual.domain;
 
 import java.util.ArrayList;
